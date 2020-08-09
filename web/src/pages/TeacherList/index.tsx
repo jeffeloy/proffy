3f93e8765a5332fbdbe6cddfa0bbe7381a8c69c9
Jefferson Eloy <jeffersoneloy08@gmail.com>
2020-08-08 21:51:19 -0300

    :bug: Fixed error when importing css file

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
 
+import "./styles.css";
+
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
